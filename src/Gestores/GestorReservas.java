a1c7c801c0a3a3f44b38accc4f2ddd54db24f555
Iker <ikcfi@plaiaundi.net>
2023-02-08 13:53:46 +0100

    sd

--- src/Gestores/GestorReservas.java
+++ src/Gestores/GestorReservas.java
@@ -1,5 +1,36 @@
 package Gestores;
 
+import java.sql.SQLException;
+import java.util.Scanner;
+
+import Datos.FormularioDatos;
+import Menus.Menuss;
+
 public class GestorReservas {
 
+	
+	public static void run(Scanner sc) throws ClassNotFoundException, SQLException {
+		int opcion_menu=0;
+		GBDD gbd = new GBDD();
+		
+		do {		
+			switch (opcion_menu) {
+			case Menuss.RESERVAR:
+				gbd.conectar();
+				//gbd.r(FormularioDatos.pedirDatosCliente(sc));
+				gbd.cerrar();
+				break;
+			case Menuss.CANCELAR_RESERVA:
+				System.out.println("segunda opcion seleccionada\n");
+				break;
+			case Menuss.SALIR:
+				System.out.println("ADIOS");
+				break;
+			default:
+				System.out.println("Opcion incorrecta!");
+			}
+			
+		} while (opcion_menu != Menuss.SALIR);
+		sc.close();
+	}
 }
