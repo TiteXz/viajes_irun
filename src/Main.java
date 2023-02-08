73c03c2ad657f9040f849d12836fc1c0725ca937
Iker <ikcfi@plaiaundi.net>
2023-02-08 13:54:04 +0100

    ya esta, por fin

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+
+import java.sql.SQLException;
+
+import Gestores.GestorViajes;
+
+public class Main {
+	public static void main(String[] args){
+		
+		GestorViajes.run();
+		//gb viene de GestorViajes
+	}
+}
