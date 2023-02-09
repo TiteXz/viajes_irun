98ef1064735db7f595379bf0bf5b36f63c18c735
Iker <ikcfi@plaiaundi.net>
2023-02-09 13:19:02 +0100

    fallitos

--- src/Gestores/GestorHabitaciones.java
+++ src/Gestores/GestorHabitaciones.java
@@ -1,5 +1,38 @@
 package Gestores;
 
+import java.util.Scanner;
+
+import Datos.FormularioDatos;
+import Menus.Menuss;
+
 public class GestorHabitaciones {
 
+	public static void run(Scanner sc) {
+		GBDD gbd = new GBDD();
+		int opcion_menu=0;
+		
+		do {		
+			Menuss.mostrarMenuHabitaciones();
+			opcion_menu = Integer.parseInt(sc.nextLine());
+			switch (opcion_menu) {
+			case Menuss.NUEVA_HABITACION:
+				gbd.conectar();
+				gbd.ainadirHabitacion(FormularioDatos.pedirIdHabitacion(sc));
+				gbd.cerrar();
+				break;
+			case Menuss.ELIMINAR_HABITACION:
+				gbd.conectar();
+				gbd.ainadirHabitacion();
+				gbd.cerrar();
+				break;
+			case Menuss.SALIR:
+				System.out.println("ADIOS");
+				break;
+			default:
+				System.out.println("Opcion incorrecta!");
+			}
+			
+		} while (opcion_menu != Menuss.SALIR);
+		sc.close();
+	}
 }
